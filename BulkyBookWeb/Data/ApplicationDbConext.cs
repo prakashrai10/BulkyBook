00281168d05a017119804542254d11413d931fba
prakashrai10 <gotnewlife1994@gmail.com>
2022-04-27 14:54:17 -0400

    Add project files.

---
+++ BulkyBookWeb/Data/ApplicationDbConext.cs
@@ -0,0 +1,12 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data;
+
+public class ApplicationDbConext :DbContext
+{
+    public ApplicationDbConext(DbContextOptions<ApplicationDbConext> options) : base(options)
+    {
+    }
+    public DbSet<Category> Categories { get; set; } 
+}
